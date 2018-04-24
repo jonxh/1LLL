43808c7665dd91bfcc6056ca53f05cff77076206
Jonathan <jonathan.hamstad@gmail.com>
2018-04-24 11:48:01 +0200

    Favicon

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="favicon.ico" />
     <script src="functions/header.js"></script>
   </head>
   <body>
